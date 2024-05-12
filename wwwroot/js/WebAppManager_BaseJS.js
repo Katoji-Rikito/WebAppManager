c48713d0a234834aace94355d071b92f9b596572
Rikito Katoji <hoangthinh99hp@gmail.com>
2024-05-12 21:05:21 +0700

    adding: base js, base controller

---
+++ wwwroot/js/WebAppManager_BaseJS.js
@@ -0,0 +1,37 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+//---------------------------------------------------------------------------------------------------
+// KHAI BÁO BIẾN ---------------------------------------------------------------------------------------------------
+//const loadPanel = $("#dxLoadingPanel").dxLoadPanel({
+//    animation: {
+//        hide: null, // Hoạt ảnh ẩn
+//        show: null, // Hoạt ảnh hiện
+//    },
+//    container: undefined, // Chỉ định vùng chứa của thành phần UI
+//    deferRendering: true, // Chỉ định xem có hiển thị nội dung của thành phần UI khi nó được hiển thị hay không. Nếu false, nội dung sẽ được hiển thị ngay lập tức
+//    delay: 0, // Sau bao lâu thì hiển thị. Tính bằng mili giây (1000ms = 1s)
+//    focusStateEnabled: true, // Chỉ định xem thành phần UI có thể được tập trung hay không
+//    height: "auto", // Chiều cao
+//    hideOnOutsideClick: false, // Ẩn nếu click vào vùng ngoài
+//    hideOnParentScroll: false, // Ẩn nếu cuộn phần tử cha
+//    hint: "Đang tải dữ liệu, vui lòng chờ trong giây lát . . .", // Chỉ định văn bản cho gợi ý xuất hiện khi người dùng tạm dừng trên thành phần UI
+//    hoverStateEnabled: true, // Chỉ định xem thành phần giao diện người dùng có thay đổi trạng thái hay không khi người dùng tạm dừng trên đó
+//    indicatorSrc: "/content/images/loading.gif", // Nguồn thay cho hình ảnh mặc định
+//    maxHeight: "50vh", // Chiều cao tối đa
+//    maxWidth: "80vw", // Chiều rộng tối đa
+//    message: "Đang tải dữ liệu...",
+//    minHeight: null, // Chiều cao tối thiểu
+//    minWidth: null, // Chiều rộng tối thiểu
+//    position: { my: "center", at: "center", of: "#mainContent" }, // Vị trí hiển thị
+//    rtlEnabled: false, // Hướng giao diện từ phải sang trái
+//    shading: true, // Tạo bóng nền hay không
+//    shadingColor: "rgba(0,0,0,0.25)", // Màu bóng nền
+//    showIndicator: true, // Hiện ảnh loading
+//    showPane: true, // Hiện bảng
+//    width: "auto", // Chiều ngang
+//}).dxLoadPanel("instance");
+
+
+// KHAI BÁO HÀM ---------------------------------------------------------------------------------------------------
