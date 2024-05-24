5c590c922748bff2cf895803475c2b4cac9be251
Rikito Katoji <hoangthinh99hp@gmail.com>
2024-05-25 00:22:03 +0700

    add: Màn hình lỗi

--- Filters/ExceptionHandler.cs
+++ Filters/ExceptionHandler.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System.Collections;
+using System.Diagnostics;
+using WebAppManager.Models;
 
 namespace WebAppManager.Filters
 {
@@ -15,8 +20,43 @@ namespace WebAppManager.Filters
         public void OnException(ExceptionContext context)
         {
             _logger.LogError(context.Exception, "Lỗi " + context.Exception.Message);
-            context.Result = new BadRequestObjectResult(new { data = context.Exception.Message });
+            //context.Result = new BadRequestObjectResult(new { data = context.Exception.Message });
+            context.Result = new ViewResult
+            {
+                ViewName = "Error",
+                ViewData = new ViewDataDictionary<ErrorViewModel>(new EmptyModelMetadataProvider(), context.ModelState)
+                {
+                    Model = new ErrorViewModel
+                    {
+                        RequestId = Activity.Current?.Id ?? context.HttpContext.TraceIdentifier,
+                        Data = ConvertToGenericDictionary(context.Exception.Data),
+                        HelpLink = context.Exception.HelpLink,
+                        HResult = context.Exception.HResult.ToString(),
+                        InnerException = context.Exception.InnerException?.ToString(),
+                        Message = context.Exception.Message,
+                        Source = context.Exception.Source,
+                        StackTrace = context.Exception.StackTrace,
+                        TargetSite = context.Exception.TargetSite?.ToString(),
+                    }
+                }
+            };
             context.ExceptionHandled = true;
+        }
+
+        /// <summary>
+        /// Chuyển IDictionary sang IDictionary<string, object>
+        /// </summary>
+        /// <param name="data">Dữ liệu cần chuyển</param>
+        /// <returns>IDictionary<string, object></returns>
+        private IDictionary<string, object> ConvertToGenericDictionary(IDictionary data)
+        {
+            var result = new Dictionary<string, object>();
+            foreach (DictionaryEntry entry in data)
+            {
+                if (entry.Key is string key)
+                    result[key] = entry.Value ?? null!;
+            }
+            return result;
         }
 
         #endregion Public Methods
--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -3,7 +3,14 @@ namespace WebAppManager.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
-
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public string? Message { get; set; }
+        public string? StackTrace { get; set; }
+        public string? InnerException { get; set; }
+        public string? TargetSite { get; set; }
+        public IDictionary<string, object>? Data { get; set; }
+        public string? Source { get; set; }
+        public string? HelpLink { get; set; }
+        public string? HResult { get; set; }
     }
 }
